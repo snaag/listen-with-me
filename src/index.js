35a97061bcf0d56eab07f350a58911532a35240c
snaag <04_miffy@naver.com>
2020-08-31 16:05:35 +0900

    :books: code: fontawesome 설정 추가

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
 import App from './App';
 import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
@@ -7,6 +11,8 @@ import { Provider } from 'react-redux';
 import reduxThunk from 'redux-thunk';
 
 import rootReducer from './modules/index';
+
+library.add(fab, fas, far);
 const store = createStore(
   rootReducer,
   composeWithDevTools(applyMiddleware(reduxThunk))
