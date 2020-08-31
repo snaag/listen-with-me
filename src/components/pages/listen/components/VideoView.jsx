e5794eb4fc89a42f7e72fc21fac749aa402c694c
snaag <04_miffy@naver.com>
2020-08-31 19:13:05 +0900

    :books: code: VideoView 기본 UI 구현

--- src/components/pages/listen/components/VideoView.jsx
+++ src/components/pages/listen/components/VideoView.jsx
@@ -1,13 +1,32 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
+import ReactPlayer from 'react-player';
 
-class VideoView extends Component {
-  render() {
-    return (
-      <div style={{ backgroundColor: 'darksalmon', height: '200px' }}>
-        VideoView
-      </div>
-    );
-  }
-}
+const VideoView = () => {
+  let videoIndex = 0;
+  const videoList = [
+    'https://www.youtube.com/watch?v=G84LnbOUqFc',
+    'https://www.youtube.com/watch?v=e413ilFWgcg',
+    'https://www.youtube.com/watch?v=3T4KGzfgjRU',
+    'https://www.youtube.com/watch?v=W8OKOIzA74o',
+  ];
+  const [currentVideoUrl, setCurrentVideoUrl] = useState(videoList[videoIndex]);
+  return (
+    <div style={{ backgroundColor: 'darksalmon', height: '200px' }}>
+      <ReactPlayer
+        style={{
+          width: '100%',
+          height: '100%',
+        }}
+        controls
+        url={currentVideoUrl}
+        onReady={() => console.log('onReady')}
+        onStart={() => console.log('onStart')}
+        onPause={() => console.log('onPause')}
+        onEnded={() => setCurrentVideoUrl(videoList[++videoIndex])}
+        onError={() => console.log('onError')}
+      />
+    </div>
+  );
+};
 
 export default VideoView;
