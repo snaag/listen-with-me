a9bfcf48aa32421ba78d311d07a7ad39494716e3
snaag <04_miffy@naver.com>
2020-09-04 14:54:25 +0900

    :shirt: style: signin, signup style 입혔음

--- src/components/navigation/components/SignIn.jsx
+++ src/components/navigation/components/SignIn.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import Modal from 'react-bootstrap/Modal';
 
+import '../../../css/Sign.css';
+
 const SignIn = ({ isActive, signIn, handleClose }) => {
   const [info, setInfo] = useState({
     email: '',
@@ -25,30 +27,43 @@ const SignIn = ({ isActive, signIn, handleClose }) => {
         <Modal.Title>로그인</Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        <button>Google 로그인</button>
-        <br />
-        <button>Kakao 로그인</button>
-        <br />
-        <hr />
-        <form onSubmit={onSignInSubmit}>
-          <label htmlFor="email">이메일</label>
-          <input
-            type="email"
-            id="email"
-            value={info.email}
-            onChange={changeInfo}
-          />
-          <br />
-          <label htmlFor="password">비밀번호</label>
-          <input
-            type="password"
-            id="password"
-            value={info.password}
-            onChange={changeInfo}
-          />
-          <br />
-          <button>로그인</button>
-        </form>
+        <div className="signin">
+          <div className="oauth">
+            <button className="oauth__google oauth__button">
+              Google 로그인
+            </button>
+            <button className="oauth__kakao oauth__button">Kakao 로그인</button>
+          </div>
+          <hr />
+          <form onSubmit={onSignInSubmit} className="signin__info">
+            <div className="info">
+              <label htmlFor="email" className="info__label">
+                이메일
+              </label>
+              <input
+                className="info__input"
+                type="email"
+                id="email"
+                value={info.email}
+                onChange={changeInfo}
+              />
+            </div>
+            <div className="info">
+              <label htmlFor="password" className="info__label">
+                비밀번호
+              </label>
+              <input
+                className="info__input"
+                type="password"
+                id="password"
+                value={info.password}
+                onChange={changeInfo}
+              />
+            </div>
+
+            <button className="signin__button sign__button">로그인</button>
+          </form>
+        </div>
       </Modal.Body>
     </Modal>
   );
