da42bfa0baebabc6ad3d7f13dbaf99694f2c3b7b
snaag <04_miffy@naver.com>
2020-09-08 11:37:53 +0900

    :shirt: style: UserTab CSS 적용함

--- src/components/tabs/UserTab.jsx
+++ src/components/tabs/UserTab.jsx
@@ -2,26 +2,25 @@ import React from 'react';
 import { withRouter } from 'react-router-dom';
 
 import '../../css/UserTab.css';
-const BASE_URL = 'http://localhost:3000';
 
 const UserTab = ({ history }) => {
   const changePage = e => {
     history.push(`/${e.target.name}`);
   };
   return (
-    <div className="tab">
+    <div className="tab d-flex justify-content-center">
       <ul className="tab__inner clearfix">
-        <li className="tab__item tab__music">
+        <li className="tab__item">
           <button onClick={changePage} name="music">
             음악
           </button>
         </li>
-        <li className="tab__item tab__playlist">
+        <li className="tab__item">
           <button onClick={changePage} name="playlist">
             내 플레이리스트
           </button>
         </li>
-        <li className="tab__item tab__profile">
+        <li className="tab__item">
           <button onClick={changePage} name="profile">
             프로필
           </button>
