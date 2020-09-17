ac51bf8593614104f1ea4db893cf2cc1efce4b4e
snaag <04_miffy@naver.com>
2020-09-17 14:37:24 +0900

    :sparkles: MainPage에서 열려있는 방에 입장 가능하도록 구현

--- src/components/pages/listen/ListenPage.jsx
+++ src/components/pages/listen/ListenPage.jsx
@@ -17,20 +17,17 @@ const ListenPage = ({
   updateMusics,
   history,
 }) => {
-  const { isHost, playListId } = history.location;
-  const [isOpen, setIsOpen] = useState(false);
-  localStorage.setItem('playlistId', playListId);
-  localStorage.setItem('isHost', isHost); // 노래 변경 시에도 써야해서, 이걸로 해줌
+  // const { isHost, playListId } = history.location;
+  // localStorage.setItem('playlistId', playListId);
+  // localStorage.setItem('isHost', isHost); // 노래 변경 시에도 써야해서, 이걸로 해줌
   const BASE_URL =
     'http://ec2-15-164-52-99.ap-northeast-2.compute.amazonaws.com:4000';
 
   const authorization = localStorage.getItem('authorization');
 
-  const getMusics = async () => {
+  const getMusics = async playListId => {
     try {
-      console.log(
-        `해당 플레이리스트(${playListId})에 대한 음악들 불러오기 시작`
-      );
+      console.log('getMusics 시작... playListId:', playListId);
 
       const { data, status } = await axios.get(`${BASE_URL}/music`, {
         headers: {
@@ -41,9 +38,8 @@ const ListenPage = ({
         },
       });
       if (status === 200) {
-        console.log(
-          `해당 플레이리스트(${playListId})에 대한 음악들 불러오기 성공`
-        );
+        console.log('getMusics 성공');
+
         // updateMusics([...data]);
         return data;
       }
@@ -55,10 +51,36 @@ const ListenPage = ({
     }
   };
 
-  const destroyRoom = async () => {
+  const getRoomStatus = async roomId => {
+    // createdAt, currentMusic_id, host_id, id, playlist_id, updatedAt
     try {
-      const roomId = localStorage.getItem('roomId');
-      console.log(`방(${roomId}) 삭제 시작`);
+      console.log('getRoomStatus 시작... roomId:', roomId);
+      const { data, status } = await axios.get(`${BASE_URL}/room`, {
+        params: {
+          id: roomId,
+        },
+        headers: {
+          authorization,
+        },
+      });
+
+      if (status === 200) {
+        console.log('getRoomStatus 성공');
+        return data;
+      } else {
+        console.log('getRoomStatus 실패');
+        console.log(data, status);
+      }
+    } catch (error) {
+      console.log('getRoomStatus 실패');
+      console.log(error.response);
+    }
+  };
+
+  const destroyRoom = async roomId => {
+    try {
+      // const roomId = localStorage.getItem('roomId');
+      console.log('destroyRoom 시작... roomId:', roomId);
       const result = await axios.delete(`${BASE_URL}/room`, {
         headers: {
           authorization,
@@ -68,23 +90,22 @@ const ListenPage = ({
         },
       });
 
-      console.log('방 삭제 중...');
       console.log(result);
-      console.log('방 삭제 성공');
+      console.log('destroyRoom 성공');
       updateMusics([]);
       return true;
 
       // return result;
     } catch (error) {
-      console.log('방 삭제 실패');
+      console.log('destroyRoom 실패');
       console.log(error.response);
       return false;
     }
   };
 
-  const createRoom = async () => {
+  const createRoom = async playListId => {
     try {
-      console.log(`방 생성 시작, playlist id ${playListId}`);
+      console.log('createRoom 시작... playListId:', playListId);
       const { data, status } = await axios.post(
         `${BASE_URL}/room`,
         {
@@ -97,46 +118,63 @@ const ListenPage = ({
         }
       );
       if (status === 201) {
-        // console.log(`방 생성 완료, roomId: ${data.id}`);
-        localStorage.setItem('roomId', data.id);
-        // setRoomId(data.id);
-        const list = await getMusics();
-        updateCurrentMusic(list[0]);
-        updateMusics([...list]);
-        setIsOpen(true);
-        // return data.id;
-        return true;
+        console.log('createRoom 성공');
+        // localStorage.setItem('roomId', data.id);
+        // const list = await getMusics();
+        // updateCurrentMusic(list[0]);
+        // updateMusics([...list]);
+        return data.id;
+      } else {
+        console.log(status, data);
+        console.log('createRoom 실패');
       }
     } catch (error) {
-      console.log('방 생성 실패');
+      console.log('createRoom 실패');
       console.log(error.response);
-      return false;
     }
   };
 
   useEffect(() => {
-    /*
-    const fn = async () => {
-      const createResult = await createRoom();
-      console.log('>createResult:', createResult);
-      if (!createResult) {
-        alert('비정상적으로 종료되었었기 때문에, 삭제 후 다시 생성합니다');
-        const destroyResult = await destroyRoom();
-        console.log('>>destroyResult:', destroyResult);
-        // const createResult = await createRoom();
-        // console.log('>>createResult:', createResult);
+    const isHost = localStorage.getItem('isHost');
+    console.log('isHost:', isHost);
+
+    const initialzeRoom = async () => {
+      if (isHost === 'true') {
+        console.log('>제가 만든 방 입니다<');
+        const playListId = localStorage.getItem('playListId');
+        // 내가 방을 연 호스트인 경우,
+        // 1. 방을 생성한다
+        const roomId = await createRoom(playListId);
+        // 2. 방의 음악 정보를 불러온다
+        const list = await getMusics(playListId);
+        updateMusics([...list]);
+        updateCurrentMusic(list[0]);
+      } else {
+        // 내가 방에 게스트로 입장한 경우,
+        // 1. 방의 음악 정보를 불러온다
+        console.log('>제가 만든 방이 아닙니다<');
+        const roomId = localStorage.getItem('roomId');
+        const { playlist_id } = await getRoomStatus(roomId);
+        const list = await getMusics(playlist_id);
+        updateMusics([...list]);
+        updateCurrentMusic(list[0]);
       }
     };
 
-    fn();
-    */
+    const finalizeRoom = async () => {
+      if (isHost === 'true') {
+        console.log('>제가 만든 방을 삭제합니다<');
 
-    if (isHost) {
-      createRoom();
-    }
+        const roomId = localStorage.getItem('roomId');
+        destroyRoom(roomId);
+      } else {
+        console.log('>게스트가 방을 나갑니다<');
+      }
+    };
+    initialzeRoom();
+
     return () => {
-      console.log('CLEAN UP');
-      if (isOpen) destroyRoom();
+      finalizeRoom();
     };
     // eslint-disable-next-line
   }, []);
